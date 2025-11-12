574fdba2eb790f779f6296e32f191f60ea0e4d7b
Andrea030105 <tummoloandrea05@gmail.com>
2025-11-12 17:23:28 +0100

    optimizatio-js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
 let eta = parseInt(prompt("Inserisci la tua età"));
 let km = parseInt(prompt("Inserisci i chilometri da percorrere"));
-let prezzo = km * 0.21;
-let sconto;
 
 
 if (eta <= 18) {
 
-    sconto = prezzo * 20 / 100;
-    prezzo = prezzo - sconto;
-    document.getElementById("prezzo").innerHTML = `${"sei minorenne, 20% di sconto"} ${prezzo.toFixed(2)}`;
+    let prezzo = (0.21 * km * 0.8).toFixed(2)
+    document.getElementById("prezzo").innerHTML = `${"sei minorenne, 20% di sconto"} ${prezzo}`;
     console.log("sei minorenne, 20% di sconto " + prezzo.toFixed(2));
 
 } else if (eta > 65) {
 
-    sconto = prezzo * 40 / 100;
-    prezzo = prezzo - sconto;
-    document.getElementById("prezzo").innerHTML = `${"sei over 65, 40% di sconto"} ${prezzo.toFixed(2)}`;
+    let prezzo = (0.21 * km * 0.6).toFixed(2)
+    document.getElementById("prezzo").innerHTML = `${"sei over 65, 40% di sconto"} ${prezzo}`;
     console.log("sei over 65, 40% di sconto " + prezzo.toFixed(2));
 
 } else {
